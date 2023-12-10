846919b599c657839b83c2ba0867a65f4fc776cc
vin <fabianuskevin147@gmail.com>
2023-12-10 13:08:41 +0700

    adding style to user acc and change bg schedule to grey-white

--- src/pages/UserAccount.jsx
+++ src/pages/UserAccount.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
+import { FaUserLock } from 'react-icons/fa';
 import useInput from '../hooks/useInput';
 import { asyncUpdatePassword, asyncDeleteUser } from '../states/users/action';
+import './styles/userAcc-style.css';
 
 const UserAccount = () => {
   const dispatch = useDispatch();
@@ -24,24 +26,27 @@ const UserAccount = () => {
   };
 
   return (
-    <div>
-      <form onSubmit={updateHandler}>
+    <div className="userPage-container">
+      <FaUserLock id="lock-img" className="fa-icon-size" size="8em" />
+      <form onSubmit={updateHandler} id="formSubmit">
         <input
+          id="pasw-input"
           type="password"
-          placeholder="password lama"
+          placeholder="Password lama"
           value={oldPassword}
           onChange={(e) => setOldPassword(e.target.value)}
         />
         <input
+          id="pasw-input"
           type="password"
-          placeholder="password baru"
+          placeholder="Password baru"
           value={newPassword}
           onChange={(e) => setNewPassword(e.target.value)}
         />
-        <button type="submit">Ubah password</button>
+        <button id="userPage-btn" type="submit">Ubah Password</button>
       </form>
-      <button type="button" onClick={deleteHandler}>
-        hapus akun
+      <button id="userPage-btn" type="button" onClick={deleteHandler}>
+        Hapus Akun
       </button>
     </div>
   );
