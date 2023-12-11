7e12ac8f3ccd356e11e34b7e764fff8a3e45cf3f
vin <fabianuskevin147@gmail.com>
2023-12-11 14:23:56 +0700

    fix style for jadwalPage

--- src/pages/JadwalPage.jsx
+++ src/pages/JadwalPage.jsx
@@ -15,30 +15,27 @@ const JadwalPage = () => {
           <div className="center-box" />
           <h4 className="about-heading">JadwalPlus</h4>
           <p className="about-text">
-            <strong>JadwalPlus</strong>
-            {' '}
-            adalah sebuah jadwal dimana pengguna  mengelola jadwal
-            dengan baik,mengorganisir dan mengatur aktivitas belajar.
-            Dalam konteks
-            yang lebih umum, JadwalPlus membantu orang
-            untuk mengelola waktu mereka secara efisien dan efektif.
+            JadwalPlus adalah sebuah jadwal dimana pengguna mengelola jadwal
+            dengan baik,mengorganisir dan mengatur aktivitas belajar. Dalam
+            konteks yang lebih umum, JadwalPlus membantu orang untuk mengelola
+            waktu mereka secara efisien dan efektif.
           </p>
           <div>
             <h2>Keuntungan Menggunakan Aplikasi JadwalPlus</h2>
             <h3 data-section-counter="1">Efesian Waktu</h3>
             <p1 className="text1">
-              Dengan aplikasi JadwalPlus, Anda dapat mengatur,
-              dan memantau kegiatan dengan lebih efisien.
+              Dengan aplikasi JadwalPlus, Anda dapat mengatur, dan memantau
+              kegiatan dengan lebih efisien.
             </p1>
             <h3 data-section-counter="2">Manajamenen Stress</h3>
             <p1>
-              Dengan memiliki JadwalPlus dapat mengurangi stres karena tahu apa yang harus dilakukan
-              dan kapan harus melakukannya.
+              Dengan memiliki JadwalPlus dapat mengurangi stres karena tahu apa
+              yang harus dilakukan dan kapan harus melakukannya.
             </p1>
             <h3 data-section-counter="3">Melacak Kemajuan Belajar</h3>
             <p1>
-              Dengan aplikasi JadwalPlus,Dapat melacak kemajuan target belajar.Ini membantu pengguna
-              untuk tetap fokus dan termotivasi.
+              Dengan aplikasi JadwalPlus,Dapat melacak kemajuan target
+              belajar.Ini membantu pengguna untuk tetap fokus dan termotivasi.
             </p1>
           </div>
         </div>
