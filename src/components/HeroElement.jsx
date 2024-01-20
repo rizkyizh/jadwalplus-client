d24ccf4fb159338f4226d8bdf6d92ca60dc52416
rizkyizh <rizkiizzulhaq14@gmail.com>
2024-01-21 02:07:50 +0700

    style(homepage): change hero, intro & benefit section

--- src/components/HeroElement.jsx
+++ src/components/HeroElement.jsx
@@ -1,11 +1,33 @@
-import React from 'react';
-import './styles/heroElement.css';
+import React from "react";
+import "./styles/heroElement.css";
+
+const stats = [
+  {
+    id: 1,
+    name: "Efesian Waktu",
+    value:
+      "Dengan aplikasi JadwalPlus, Anda dapat mengatur, dan memantau kegiatan dengan lebih efisien",
+  },
+  {
+    id: 2,
+    name: "Manajemen Stress",
+    value:
+      "Dengan memiliki JadwalPlus dapat mengurangi stres karena tahu apa yang harus dilakukan dan kapan harus melakukannya",
+  },
+  {
+    id: 3,
+    name: "annually",
+    value:
+      "Dengan aplikasi JadwalPlus, Dapat melacak kemajuan target belajar. Ini membantu pengguna untuk tetap fokus dan termotivasi",
+  },
+];
 
 const CombinedComponent = () => {
   return (
     <>
-      <section className="mt-6 flex flex-col lg:flex-row items-center justify-center gap-2 p-8">
-        <div className="text-2xl">
+      {/* hero-start */}
+      <section className="hero mt-6 flex flex-col lg:flex-row items-center justify-center gap-2 p-8">
+        <div className="text-2xl font-extrabold sm:text-3xl">
           <p>
             Taklukkan Waktu, Atur Hidup
             <br />
@@ -17,44 +39,55 @@ const CombinedComponent = () => {
             dengan JadwalPlus
           </p>
         </div>
-        <img alt="model" className="w-[95%] lg:w-[50%] xl:w-[45%]" src="/image/foto.jpeg" />
+        <img
+          alt="model"
+          className="w-[95%] lg:w-[50%] xl:w-[45%]"
+          src="/image/foto.jpeg"
+        />
       </section>
-      <section className="p-8 lg:flex lg:justify-center lg:items-center gap-3 lg:gap-8 lg:w-[80%] lg:mx-auto">
-        <div className="flex lg:flex-col items-center gap-6">
+      {/* hero-end */}
+      {/* intro-start */}
+      <section className="p-8 gap-6 mx-auto lg:w-3/4 lg:flex lg:justify-center lg:items-center">
+        <div className="flex lg:flex-col items-center justify-center mr-3">
           <img
             src="./image/task1.png"
             alt="Ilustrasi JadwalPlus"
-            className="w-[100px] md:w-[150px]"
+            className="mr-3 w-[100px] md:w-[150px]"
           />
           <h4 className="text-2xl font-semibold">JadwalPlus</h4>
         </div>
-        <p className="mt-5 text-base">
-          JadwalPlus adalah sebuah jadwal dimana pengguna mengelola jadwal
-          dengan baik, mengorganisir dan mengatur aktivitas belajar. Dalam
-          konteks yang lebih umum, JadwalPlus membantu orang untuk mengelola
-          waktu mereka secara efisien dan efektif.
+        <p className="mt-5 text-lg">
+          <span className="font-bold">JadwalPlus</span> adalah sebuah jadwal
+          dimana pengguna mengelola jadwal dengan baik, mengorganisir dan
+          mengatur aktivitas belajar. Dalam konteks yang lebih umum, JadwalPlus
+          membantu orang untuk mengelola waktu mereka secara efisien dan
+          efektif.
         </p>
       </section>
+      {/* intro-end */}
+      {/* benefit-start */}
       <section className="mb-6">
-        <div className="aboutText-points lg:w-[80%] mx-auto">
-          <h2 className="font-semibold">Keuntungan Menggunakan Aplikasi JadwalPlus</h2>
-          <h3 data-section-counter="1" className="text-lg">Efesian Waktu</h3>
-          <p1 className="text1">
-            Dengan aplikasi JadwalPlus, Anda dapat mengatur, dan memantau
-            kegiatan dengan lebih efisien.
-          </p1>
-          <h3 data-section-counter="2" className="text-lg">Manajemen Stress</h3>
-          <p1 className="text1">
-            Dengan memiliki JadwalPlus dapat mengurangi stres karena tahu apa
-            yang harus dilakukan dan kapan harus melakukannya.
-          </p1>
-          <h3 data-section-counter="3" className="text-lg">Melacak Kemajuan Belajar</h3>
-          <p1 className="text1">
-            Dengan aplikasi JadwalPlus, Dapat melacak kemajuan target
-            belajar. Ini membantu pengguna untuk tetap fokus dan termotivasi.
-          </p1>
+        <div className="bg-white py-24 sm:py-32">
+          <div className="mx-auto max-w-7xl px-6 lg:px-8">
+            <dl className="grid grid-cols-1 gap-x-8 gap-y-16 text-center lg:grid-cols-3">
+              {stats.map((stat) => (
+                <div
+                  key={stat.id}
+                  className="mx-auto flex max-w-xs flex-col gap-y-4"
+                >
+                  <dt className="text-base leading-7 text-gray-600">
+                    {stat.value}
+                  </dt>
+                  <dd className="order-first text-2xl font-semibold tracking-tight text-gray-900 sm:text-3xl">
+                    {stat.name}
+                  </dd>
+                </div>
+              ))}
+            </dl>
+          </div>
         </div>
       </section>
+      {/* benefit-end */}
     </>
   );
 };
