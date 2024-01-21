ecaa620daa6d6c85bc77cf821cb4bf3f28e19a05
rizkyizh <rizkiizzulhaq14@gmail.com>
2024-01-21 10:26:20 +0700

    style(about page): recast style

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,198 +1,123 @@
-import React from 'react';
-import { FaInstagram, FaGithub, FaLinkedin } from 'react-icons/fa';
-import './styles/about.css';
+import React from "react";
+import { FaInstagram, FaGithub, FaLinkedin } from "react-icons/fa";
+
+const people = [
+  {
+    name: "Munawar Amin",
+    role: "Frontend & UI/UX",
+    sosmed: {
+      instagram: "https://www.instagram.com/munawar_amin2/",
+      github: "https://github.com/munawaramin2002",
+      linkedin: "https://www.linkedin.com/in/munawar-amin/",
+    },
+    imageUrl: "./image/amin3.png",
+  },
+  {
+    name: "Restu",
+    role: "Frontend & UI/UX",
+    sosmed: {
+      instagram: "https://www.instagram.com/restu_m.s/",
+      github: "https://github.com/RestuMS",
+      linkedin: "https://www.linkedin.com/in/restu-muhammad-saputro-402888299/",
+    },
+    imageUrl: "./image/restums.jpg",
+  },
+  {
+    name: "Ade Lintang",
+    role: "Frontend & Backend",
+    sosmed: {
+      instagram: "https://www.instagram.com/im.adelintang/",
+      github: "https://github.com/adelintang",
+      linkedin: "https://www.linkedin.com/in/ade-lintang-permono-2b4a0b29a/",
+    },
+    imageUrl: "./image/ade.jpg",
+  },
+  {
+    name: "Kevin",
+    role: "Frontend",
+    sosmed: {
+      instagram: "https://www.instagram.com/fkvin_/",
+      github: "https://github.com/blueskye16",
+      linkedin: "https://www.linkedin.com/in/fabianus-kevin-406285231/",
+    },
+    imageUrl: "./image/kevin.jpeg",
+  },
+  {
+    name: "Hikmalul",
+    role: "Backend & DevOps",
+    sosmed: {
+      instagram: "https://www.instagram.com/hikmal_a.s/",
+      github: "https://github.com/MituBaby",
+      linkedin: "https://www.linkedin.com/in/hikmalul-a-la-978a07299/",
+    },
+    imageUrl: "./image/alul.jpeg",
+  },
+];
 
 const AboutPage = () => {
   return (
-    <div className="mt-6 mx-auto lg:w-[80%]">
-      <section className="p-8 lg:flex lg:justify-center lg:items-center gap-3 lg:gap-8 lg:w-[80%] lg:mx-auto">
-        <div className="flex lg:flex-col items-center gap-6">
-          <img
-            src="./image/task1.png"
-            alt="Ilustrasi JadwalPlus"
-            className=""
-          />
-          <h4 className="text-2xl font-semibold">JadwalPlus</h4>
+    <div className="bg-white py-24 sm:py-32">
+      <div className="mx-auto grid max-w-7xl gap-x-8 gap-y-20 px-6 lg:px-8 xl:grid-cols-2">
+        <div className="max-w-2xl">
+          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+            Tim Kami
+          </h2>
+          <p className="mt-6 text-lg leading-8 text-gray-600">
+            "Jangan biarkan waktu mengatur kita, tetapi kitalah yang mengatur
+            waktu yang ada"
+          </p>
         </div>
-        <p className="mt-5 text-base">
-          JadwalPlus mengajak kepada pengguna untuk mengatur jadwal lebih efisien,
-          memenejemen waktu lebih mudah lagi. Tujuan utama dari JadwalPlus sendiri
-          yaitu untuk mentracking aktivitas sehari-hari, terutama dalam era modern
-          di mana kehidupan kita semakin sibuk dan kompleks.
-        </p>
-      </section>
-      <div className="team-section mb-8 p-4 sm:p-8">
-        <div className="team-heading-container">
-          <h4 className="team-heading text-2xl font-semibold">Tim Kami</h4>
-          <div className="team-line" />
-        </div>
-        <div className="member-container grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
-          <div className="member-box">
-            <div className="member-content">
-              <img src="./image/amin3.png" alt="Foto amin" className="member-image photo" />
-              <h4>Munawar Amin</h4>
-              <p>(Frontend & UI/UX)</p>
-            </div>
-            <div className="social-icons">
-              <a
-                href="https://www.instagram.com/munawar_amin2/"
-                target="_blank"
-                rel="noopener noreferrer"
-                aria-label="instagram-account"
-              >
-                <FaInstagram />
-              </a>
-              <a
-                href="https://github.com/munawaramin2002"
-                target="_blank"
-                rel="noopener noreferrer"
-                aria-label="github-account"
-              >
-                <FaGithub />
-              </a>
-              <a
-                href="https://www.linkedin.com/in/munawar-amin/"
-                target="_blank"
-                rel="noopener noreferrer"
-                aria-label="linkedin-account"
-              >
-                <FaLinkedin />
-              </a>
-            </div>
-          </div>
-          <div className="member-box">
-            <div className="member-content">
-              <img src="./image/restums.jpg" alt="Foto restu" className="member-image photo" />
-              <h4>Restu</h4>
-              <p>(Frontend & UI/UX)</p>
-            </div>
-            <div className="social-icons">
-              <a
-                href="https://www.instagram.com/restu_m.s/"
-                target="_blank"
-                rel="noopener noreferrer"
-                aria-label="instagram-account"
-              >
-                <FaInstagram />
-              </a>
-              <a
-                href="https://github.com/RestuMS"
-                target="_blank"
-                rel="noopener noreferrer"
-                aria-label="github-account"
-              >
-                <FaGithub />
-              </a>
-              <a
-                href="https://www.linkedin.com/in/restu-muhammad-saputro-402888299/"
-                target="_blank"
-                rel="noopener noreferrer"
-                aria-label="linkedin-account"
-              >
-                <FaLinkedin />
-              </a>
-            </div>
-          </div>
-          <div className="member-box">
-            <div className="member-content">
-              <img src="./image/ade.jpg" alt="Foto Ade" className="member-image photo" />
-              <h4>Ade Lintang</h4>
-              <p>(Frontend & Backend)</p>
-            </div>
-            <div className="social-icons">
-              <a
-                href="https://www.instagram.com/im.adelintang/"
-                target="_blank"
-                rel="noopener noreferrer"
-                aria-label="instagram-account"
-              >
-                <FaInstagram />
-              </a>
-              <a
-                href="https://github.com/adelintang"
-                target="_blank"
-                rel="noopener noreferrer"
-                aria-label="github-account"
-              >
-                <FaGithub />
-              </a>
-              <a
-                href="https://www.linkedin.com/in/ade-lintang-permono-2b4a0b29a/"
-                target="_blank"
-                rel="noopener noreferrer"
-                aria-label="linkedin-account"
-              >
-                <FaLinkedin />
-              </a>
-            </div>
-          </div>
-          <div className="member-box">
-            <div className="member-content">
-              <img id="kevin-photo" src="./image/kevin.jpeg" alt="Foto Kevin" className="member-image photo" />
-              <h4>Kevin</h4>
-              <p>(Frontend)</p>
-            </div>
-            <div className="social-icons">
-              <a
-                href="https://www.instagram.com/fkvin_/"
-                target="_blank"
-                rel="noopener noreferrer"
-                aria-label="instagram-account"
-              >
-                <FaInstagram />
-              </a>
-              <a
-                href="https://github.com/blueskye16"
-                target="_blank"
-                rel="noopener noreferrer"
-                aria-label="github-account"
-              >
-                <FaGithub />
-              </a>
-              <a
-                href="https://www.linkedin.com/in/fabianus-kevin-406285231/"
-                target="_blank"
-                rel="noopener noreferrer"
-                aria-label="linkedin-account"
-              >
-                <FaLinkedin />
-              </a>
-            </div>
-          </div>
-          <div className="member-box">
-            <div className="member-content">
-              <img src="./image/alul.jpeg" alt="Foto kevin" className="member-image photo" />
-              <h4>Hikmalul</h4>
-              <p>(Backend & DevOps)</p>
-            </div>
-            <div className="social-icons">
-              <a
-                href="https://www.instagram.com/hikmal_a.s/"
-                target="_blank"
-                rel="noopener noreferrer"
-                aria-label="instagram-account"
-              >
-                <FaInstagram />
-              </a>
-              <a
-                href="https://github.com/MituBaby"
-                target="_blank"
-                rel="noopener noreferrer"
-                aria-label="github-account"
-              >
-                <FaGithub />
-              </a>
-              <a
-                href="https://www.linkedin.com/in/hikmalul-a-la-978a07299/"
-                target="_blank"
-                rel="noopener noreferrer"
-                aria-label="linkedin-account"
-              >
-                <FaLinkedin />
-              </a>
-            </div>
-          </div>
-        </div>
+        <ul
+          role="list"
+          className="grid gap-x-8 gap-y-12 sm:grid-cols-2 sm:gap-y-16 xl:col-span-2"
+        >
+          {people.map((person) => (
+            <li key={person.name}>
+              <div className="flex items-center gap-x-6">
+                <img
+                  className="h-24 w-24 rounded-full bg-cover"
+                  src={person.imageUrl}
+                  alt=""
+                />
+                <div>
+                  <h3 className="text-base font-semibold leading-7 tracking-tight text-gray-900">
+                    {person.name}
+                  </h3>
+                  <p className="text-sm font-semibold leading-6 text-indigo-600">
+                    {person.role}
+                  </p>
+                  <div className="social-icons flex my-2">
+                    <a
+                      href={person.sosmed.instagram}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      aria-label="instagram-account"
+                    >
+                      <FaInstagram />
+                    </a>
+                    <a
+                      href={person.sosmed.github}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      aria-label="github-account"
+                      className="mx-2 block"
+                    >
+                      <FaGithub />
+                    </a>
+                    <a
+                      href={person.sosmed.linkedin}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      aria-label="linkedin-account"
+                    >
+                      <FaLinkedin />
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   );
